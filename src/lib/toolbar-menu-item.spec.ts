7bcc213a10e3d9855bcb86d2a8418cd0450e2ab7
Matthew Davis <matthew@matthewdavis.io>
2020-02-26 01:26:49 -0500

    bump

--- src/lib/toolbar-menu-item.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ToolbarMenuItem } from './toolbar-menu-item';
-
-describe('ToolbarMenuItem', () => {
-  it('should create an instance', () => {
-    expect(new ToolbarMenuItem()).toBeTruthy();
-  });
-});
--- src/public-api.ts
+++ src/public-api.ts
@@ -5,4 +5,5 @@
 export * from './lib/toolbar.service';
 export * from './lib/toolbar.component';
 export * from './lib/toolbar.module';
+export * from './lib/toolbar-menu-item';
 export * from './lib/toolbar';
